82c960227ba42c4bb59f6a2d81bbc7917bf5b7d3
Pascal Pfiffner <phase.of.matter@gmail.com>
2011-07-22 14:29:10 -0400

    2 spaces -> tabs

--- ui/jmvc/ui/controllers/carenet_controller.js
+++ ui/jmvc/ui/controllers/carenet_controller.js
@@ -9,138 +9,138 @@
 $.Controller.extend('UI.Controllers.Carenet',
 /* @Static */
 { 
-  onDocument: true,
-  label: null,
-  accounts: {},
-  carenets: [],
-  /**
-   * Show and manipulate carenets for this record
-   */
-   show: function() {
-     var _this = this;
+	onDocument: true,
+	label: null,
+	accounts: {},
+	carenets: [],
+	/**
+	* Show and manipulate carenets for this record
+	*/
+    show: function() {
+		var _this = this;
 
-     UI.Models.Record.get(RecordController.RECORD_ID, null, function(record) {
-       _this.label = record.label;
+		UI.Models.Record.get(RecordController.RECORD_ID, null, function(record) {
+			_this.label = record.label;
 
-       record.get_carenets(null, function(carenets) {
-         $(carenets).each(function(i, carenet) {
-             carenet.get_people(function(account_objects_list) {
-               _this.accounts[carenet.carenet_id] = account_objects_list;
-             });
-         });
-         
-         var check_and_go = function() {
-           // console.log('1: ', carenets.length == 0)
-           // console.log('2: ', _.all(_.map(carenets, function(i, c) {return accounts[c.carenet_id] != null;})))
-           if (carenets.length == 0 || _.all(_.map(carenets, function(carenet, i) {return _this.accounts[carenet.carenet_id] != null;}))) {
-             _this.carenets = carenets;
-             $('#app_content').html(
-               $.View('//ui/views/carenet/show.ejs',
-                      {'label': _this.label,
-                       'carenets': _this.carenets,
-                       'accounts': _this.accounts }))
-             $('#app_content_iframe').hide();
-             $('#app_content').show();
+			record.get_carenets(null, function(carenets) {
+				$(carenets).each(function(i, carenet) {
+						carenet.get_people(function(account_objects_list) {
+							_this.accounts[carenet.carenet_id] = account_objects_list;
+						});
+				});
+				
+				var check_and_go = function() {
+					// console.log('1: ', carenets.length == 0)
+					// console.log('2: ', _.all(_.map(carenets, function(i, c) {return accounts[c.carenet_id] != null;})))
+					if (carenets.length == 0 || _.all(_.map(carenets, function(carenet, i) {return _this.accounts[carenet.carenet_id] != null;}))) {
+						_this.carenets = carenets;
+						$('#app_content').html(
+							$.View('//ui/views/carenet/show.ejs',
+											{'label': _this.label,
+											'carenets': _this.carenets,
+											'accounts': _this.accounts }))
+						$('#app_content_iframe').hide();
+						$('#app_content').show();
 
-             $('.remove_account').click(function(e) {
-               var carenet_and_account = this.id.split('|');
-               var carenet_id = carenet_and_account[0];
-               var account_id = carenet_and_account[1];
-               UI.Controllers.Carenet.remove_account(carenet_id, account_id);
-               UI.Controllers.Carenet.show();
-             });
+						$('.remove_account').click(function(e) {
+							var carenet_and_account = this.id.split('|');
+							var carenet_id = carenet_and_account[0];
+							var account_id = carenet_and_account[1];
+							UI.Controllers.Carenet.remove_account(carenet_id, account_id);
+							UI.Controllers.Carenet.show();
+						});
 
-             $('.remove_carenet').click(function(e) {
-               var id_and_name = this.id.split('|');
-               UI.Controllers.Carenet.remove_carenet(id_and_name[0], id_and_name[1]);
-               UI.Controllers.Carenet.show();
-             });
+						$('.remove_carenet').click(function(e) {
+							var id_and_name = this.id.split('|');
+							UI.Controllers.Carenet.remove_carenet(id_and_name[0], id_and_name[1]);
+							UI.Controllers.Carenet.show();
+						});
 
-             $('#add_carenet_account_form').submit(function(e) {
-               var carenet_id = $(this).find('[name=carenet_id]').val();
-               var account_id = $(this).find('[name=account_id]').val();
-               UI.Controllers.Carenet.add_account(carenet_id, account_id);
-               UI.Controllers.Carenet.show();
-               return false;
-             });
+						$('#add_carenet_account_form').submit(function(e) {
+							var carenet_id = $(this).find('[name=carenet_id]').val();
+							var account_id = $(this).find('[name=account_id]').val();
+							UI.Controllers.Carenet.add_account(carenet_id, account_id);
+							UI.Controllers.Carenet.show();
+							return false;
+						});
 
-             $('#new_carenet_form').submit(function(e) {
-               var carenet_name = $(this).find('[name=new_carenet_name]').val();
-               UI.Controllers.Carenet.create_carenet(carenet_name);
-               UI.Controllers.Carenet.show();
-               return false;
-             });
+						$('#new_carenet_form').submit(function(e) {
+							var carenet_name = $(this).find('[name=new_carenet_name]').val();
+							UI.Controllers.Carenet.create_carenet(carenet_name);
+							UI.Controllers.Carenet.show();
+							return false;
+						});
 
-             $('#rename_carenet_form').submit(function(e) {
-               var id = $('#rename_carenet_form select').first().val();
-               var carenet_name = $(this).find('[name=new_name]').val();
-               UI.Controllers.Carenet.rename_carenet(id, carenet_name);
-               UI.Controllers.Carenet.show();
-               return false;
-             });
+						$('#rename_carenet_form').submit(function(e) {
+							var id = $('#rename_carenet_form select').first().val();
+							var carenet_name = $(this).find('[name=new_name]').val();
+							UI.Controllers.Carenet.rename_carenet(id, carenet_name);
+							UI.Controllers.Carenet.show();
+							return false;
+						});
 
-           } else {
-             _.delay(check_and_go, 200); // re-queue self
-           }
-         }
+					} else {
+						_.delay(check_and_go, 200); // re-queue self
+					}
+				}
 
-         _.delay(check_and_go, 200); // first call with delay to serialize with get_carenets above
-       });
-     });
-   },
+				_.delay(check_and_go, 200); // first call with delay to serialize with get_carenets above
+			});
+		});
+	},
 
-   get_carenet: function(carenet_id) {
-     return new UI.Models.Carenet({
-       'record_id': RecordController.RECORD_ID,
-       'carenet_id': carenet_id,
-       'name': null
-     })
-   },
+	get_carenet: function(carenet_id) {
+		return new UI.Models.Carenet({
+			'record_id': RecordController.RECORD_ID,
+			'carenet_id': carenet_id,
+			'name': null
+		})
+	},
 
-  create_carenet: function(name) {
-    UI.Models.Record.get(RecordController.RECORD_ID, null, function(record) {
-      record.create_carenet(name);
-    })
-    return false;
-  },
+	create_carenet: function(name) {
+		UI.Models.Record.get(RecordController.RECORD_ID, null, function(record) {
+			record.create_carenet(name);
+		})
+		return false;
+	},
 
-   remove_carenet: function(carenet_id, carenet_name) {
-     if (confirm('Are you sure you want to remove the ' + carenet_name + ' carenet?')) {
-       UI.Models.Record.get(RecordController.RECORD_ID, null, function(record) {
-         record.remove_carenet(carenet_id);
-       })
-     }
-     return false;
-   },
+	remove_carenet: function(carenet_id, carenet_name) {
+		if (confirm('Are you sure you want to remove the ' + carenet_name + ' carenet?')) {
+			UI.Models.Record.get(RecordController.RECORD_ID, null, function(record) {
+				record.remove_carenet(carenet_id);
+			})
+		}
+		return false;
+	},
 
-   rename_carenet: function(carenet_id, name) {
-     UI.Models.Record.get(RecordController.RECORD_ID, null, function(record) {
-       record.rename_carenet(carenet_id, name);
-     })
-     return false;
-   },
+	rename_carenet: function(carenet_id, name) {
+		UI.Models.Record.get(RecordController.RECORD_ID, null, function(record) {
+			record.rename_carenet(carenet_id, name);
+		})
+		return false;
+	},
 
-   remove_account: function(carenet_id, account_id) {
-     var carenet = this.get_carenet(carenet_id);
-     if (confirm('Are you sure you want to remove ' + account_id + ' ?')) { carenet.remove_account(account_id); }
-     return false;
-   },
+	remove_account: function(carenet_id, account_id) {
+		var carenet = this.get_carenet(carenet_id);
+		if (confirm('Are you sure you want to remove ' + account_id + ' ?')) { carenet.remove_account(account_id); }
+		return false;
+	},
 
-   add_account: function(carenet_id, account_id) {
-     var carenet = this.get_carenet(carenet_id);
-     if (confirm('Are you sure you want to share with ' + account_id + ' ?')) {
-       var callback = function(data){
-         if ($(data).find('ok').length !== 1) { alert('Could not add the "'+account_id+'" to the carenet, please try again.')}
-       }
-       carenet.add_account(account_id, callback);
-     }
-     return false;
-   }
+	add_account: function(carenet_id, account_id) {
+		var carenet = this.get_carenet(carenet_id);
+		if (confirm('Are you sure you want to share with ' + account_id + ' ?')) {
+			var callback = function(data){
+				if ($(data).find('ok').length !== 1) { alert('Could not add the "'+account_id+'" to the carenet, please try again.')}
+			}
+			carenet.add_account(account_id, callback);
+		}
+		return false;
+	}
 },
 /* @Prototype */
 {
     /**
      * Click bound to #carenet i.e. the "Sharing" link
      */
-     "click": function() { UI.Controllers.Carenet.show() }
+    "click": function() { UI.Controllers.Carenet.show() }
 });
--- ui/jmvc/ui/controllers/pha_controller.js
+++ ui/jmvc/ui/controllers/pha_controller.js
@@ -25,179 +25,179 @@
 $.Controller.extend('UI.Controllers.PHA',
 /* @Static */
 {
-  onDocument: true,
-   /**
-   * Show and manipulate phas for this record. Refactor!
-   */
-   show: function() {
-     this.record_info = RecordController.RECORDS[RecordController.RECORD_ID]; // get the record info from the globals (FIXME later)
-     this.carenet_phas_active_ids = {};
-     var _this = this;
-     var record = null;
-     var all_phas = null;
-     var local_phas = null;
-     var carenets = null;
+	onDocument: true,
+	/**
+	* Show and manipulate phas for this record. Refactor!
+	*/
+	show: function() {
+		this.record_info = RecordController.RECORDS[RecordController.RECORD_ID]; // get the record info from the globals (FIXME later)
+		this.carenet_phas_active_ids = {};
+		var _this = this;
+		var record = null;
+		var all_phas = null;
+		var local_phas = null;
+		var carenets = null;
 
-     _show_template = function(){
-       var params = {
-         'local_phas': _this.local_phas,
-         'record_info': _this.record_info,
-         'carenets': _this.carenets,
-         'carenet_phas_active_ids': _this.carenet_phas_active_ids
-       };
-       $('#app_content').html($.View('//ui/views/pha/show.ejs', params));
-       $('#app_content_iframe').hide();
-       $('#app_content').show();
-     };
-     
-     _add_handlers = function(){
-       $('.remove_app').click(function(evt) {
-         UI.Models.PHA.delete_pha(_this.record_info.id, evt.target.id, function(){
-           UI.Controllers.PHA.show(); // reload view
-           UI.Controllers.MainController._remove_app(evt.target.id); // remove app from selector
-         })
-       });
+		_show_template = function(){
+			var params = {
+				'local_phas': _this.local_phas,
+				'record_info': _this.record_info,
+				'carenets': _this.carenets,
+				'carenet_phas_active_ids': _this.carenet_phas_active_ids
+			};
+			$('#app_content').html($.View('//ui/views/pha/show.ejs', params));
+			$('#app_content_iframe').hide();
+			$('#app_content').show();
+		};
+		
+		_add_handlers = function(){
+			$('.remove_app').click(function(evt) {
+				UI.Models.PHA.delete_pha(_this.record_info.id, evt.target.id, function(){
+					UI.Controllers.PHA.show(); // reload view
+					UI.Controllers.MainController._remove_app(evt.target.id); // remove app from selector
+				})
+			});
 
-       $('#pha_carenets_form').submit(function(evt) {
-         var pha_id = $(this).find('input[type=hidden]').attr('value');
-         var local_pha = _(_this.local_phas).detect(function(l){ return l.id === pha_id; });
-         var done = function(){$('#update_carenets').val('{% trans "Updated!" %}')}
-         
-         $(this).find('input[name=carenet]').each(function(i, checkbox) {
-           var carenet = _(_this.carenets).detect(function(c){ return c.carenet_id === checkbox.value; })
-           if (checkbox.checked) { carenet.add_pha(local_pha, done); }
-           else { carenet.remove_pha(local_pha, done); }
-         });
-         _.delay(function(){UI.Controllers.PHA.show()}, 1000); // reload view
-         return false;
-       });
-     };
-     
-     var record_load_callback = function(record) {
-       // get all the phas
-       UI.Models.PHA.get_all(function(all_phas) {
-         _this.all_phas = all_phas;
-         
-         var after_pha_callback = function(local_phas) {
-           // local_phas are the phas for this record
-           _this.local_phas = local_phas;
+			$('#pha_carenets_form').submit(function(evt) {
+				var pha_id = $(this).find('input[type=hidden]').attr('value');
+				var local_pha = _(_this.local_phas).detect(function(l){ return l.id === pha_id; });
+				var done = function(){$('#update_carenets').val('{% trans "Updated!" %}')}
+				
+				$(this).find('input[name=carenet]').each(function(i, checkbox) {
+					var carenet = _(_this.carenets).detect(function(c){ return c.carenet_id === checkbox.value; })
+					if (checkbox.checked) { carenet.add_pha(local_pha, done); }
+					else { carenet.remove_pha(local_pha, done); }
+				});
+				_.delay(function(){UI.Controllers.PHA.show()}, 1000); // reload view
+				return false;
+			});
+		};
+		
+		var record_load_callback = function(record) {
+			// get all the phas
+			UI.Models.PHA.get_all(function(all_phas) {
+				_this.all_phas = all_phas;
+				
+				var after_pha_callback = function(local_phas) {
+					// local_phas are the phas for this record
+					_this.local_phas = local_phas;
 
-           // get the carenets for the current record
-           _this.record.get_carenets(null, function(carenets) {
-             _this.carenets = carenets;
+					// get the carenets for the current record
+					_this.record.get_carenets(null, function(carenets) {
+						_this.carenets = carenets;
 
-             // get the apps for each carenet
-             $(carenets).each(function(i, carenet) {
-                UI.Models.PHA.get_by_carenet(carenet.carenet_id, null, function(carenet_phas) {
+						// get the apps for each carenet
+						$(carenets).each(function(i, carenet) {
+								UI.Models.PHA.get_by_carenet(carenet.carenet_id, null, function(carenet_phas) {
 
-                  var _set_active_ids = function(callback){
-                    _(carenet_phas).each(function(c_pha){
-                      // if this _carenet_ pha is also in local_phas, append it to a item carenet_phas_active_ids object (keyed by carenet_id)
-                      if (_(_this.local_phas).detect(function(p){ return p.id === c_pha.id; })) {
-                        if (!_this.carenet_phas_active_ids[carenet.carenet_id]) { _this.carenet_phas_active_ids[carenet.carenet_id] = [c_pha.id]; }
-                        else { _this.carenet_phas_active_ids[carenet.carenet_id].push(c_pha.id); }
-                      }
-                    })
-                    
-                    callback();
-                  } // _set_active_ids
+									var _set_active_ids = function(callback){
+										_(carenet_phas).each(function(c_pha){
+											// if this _carenet_ pha is also in local_phas, append it to a item carenet_phas_active_ids object (keyed by carenet_id)
+											if (_(_this.local_phas).detect(function(p){ return p.id === c_pha.id; })) {
+												if (!_this.carenet_phas_active_ids[carenet.carenet_id]) { _this.carenet_phas_active_ids[carenet.carenet_id] = [c_pha.id]; }
+												else { _this.carenet_phas_active_ids[carenet.carenet_id].push(c_pha.id); }
+											}
+										})
+										
+										callback();
+									} // _set_active_ids
 
-                  _set_active_ids(_show_template);
-                  _add_handlers();
+									_set_active_ids(_show_template);
+									_add_handlers();
 
-               }); // get_by_carenet
-             }); // each carenet
-           }); // get carenets
-         }; // end after_pha_callback
+							}); // get_by_carenet
+						}); // each carenet
+					}); // get carenets
+				}; // end after_pha_callback
 
-         // is this a carenet or a record? depending on which get the associated apps
-         if (_this.record_info.carenet_id) { UI.Models.PHA.get_by_carenet(_this.record_info.carenet_id, null, after_pha_callback); }
-         else { UI.Models.PHA.get_by_record(record.record_id, null, after_pha_callback) }
-       });
-     }; // end record_load_callback
+				// is this a carenet or a record? depending on which get the associated apps
+				if (_this.record_info.carenet_id) { UI.Models.PHA.get_by_carenet(_this.record_info.carenet_id, null, after_pha_callback); }
+				else { UI.Models.PHA.get_by_record(record.record_id, null, after_pha_callback) }
+			});
+		}; // end record_load_callback
 
-     // load the record from the model and call the callback
-     UI.Models.Record.get(RecordController.RECORD_ID, _this.record_info.carenet_id, function(record) {
-       _this.record = record;
-       record_load_callback(record);
-     });
-   
-   
-   }
+		// load the record from the model and call the callback
+		UI.Models.Record.get(RecordController.RECORD_ID, _this.record_info.carenet_id, function(record) {
+			_this.record = record;
+			record_load_callback(record);
+		});
+	
+	
+	}
 },
 /* @Prototype */
 {
-     "click": function() { UI.Controllers.PHA.show() }
+    "click": function() { UI.Controllers.PHA.show() }
 });
 
 
 // OLD: includes carenet stuff and view
 
 // PHAController= MVC.Controller.extend('pha', {
-//     index: function(params) {
-//     var _this = this;
-//     
-//     _this.phas = {};
-//     _this.active_carenets = [];
-//     
-//     var record_id = params.record_id;
-//     var pha_id = params.pha_id;
-//     
-//     // get the record
-//     Record.get(record_id, null, function(record) {
-//         _this.record = record;
-//         
-//         // get the carenets for the current record
-//         record.get_carenets(null, function(carenets) {
-//         _this.carenets = carenets;
-//         
-//         // get the apps for each carenet
-//         $(carenets).each(function(i, carenet) {
-//             PHA.get_by_carenet(carenet.carenet_id, null, function(p) {
-//             _this.phas[carenet.carenet_id] = p;
-//             
-//             // check if the current PHA is in here
-//             if (_.detect(p, function(one_pha) {
-//                 return one_pha.id == pha_id;
-//             })) {
-//                 _this.active_carenets.push(carenet.carenet_id);
-//             }
-//             });
-//         });
-//         });
-//     });
-//     
-//     // get the app description
-//     PHA.get(record_id, pha_id, function(pha) {
-//         _this.pha = pha;
-//     });
-//     
-//     // display the page
-//     when_ready(function() {
-//         return (_this.pha && _this.record && _this.carenets && _.map(_this.carenets, function(i, c) {return _this.phas[c.carenet_id] != null;}));
-//     }, function() {
-//         $('#app_content_iframe').fadeOut(function(){
-//         $('#app_content').fadeIn(function(){
-//             _this.render({to: 'app_content'});
-//         });
-//         });
+//		index: function(params) {
+//		var _this = this;
+//		
+//		_this.phas = {};
+//		_this.active_carenets = [];
+//		
+//		var record_id = params.record_id;
+//		var pha_id = params.pha_id;
+//		
+//		// get the record
+//		Record.get(record_id, null, function(record) {
+//				_this.record = record;
+//				
+//				// get the carenets for the current record
+//				record.get_carenets(null, function(carenets) {
+//				_this.carenets = carenets;
+//				
+//				// get the apps for each carenet
+//				$(carenets).each(function(i, carenet) {
+//						PHA.get_by_carenet(carenet.carenet_id, null, function(p) {
+//						_this.phas[carenet.carenet_id] = p;
+//						
+//						// check if the current PHA is in here
+//						if (_.detect(p, function(one_pha) {
+//								return one_pha.id == pha_id;
+//						})) {
+//								_this.active_carenets.push(carenet.carenet_id);
+//						}
+//						});
+//				});
+//				});
+//		});
+//		
+//		// get the app description
+//		PHA.get(record_id, pha_id, function(pha) {
+//				_this.pha = pha;
+//		});
+//		
+//		// display the page
+//		when_ready(function() {
+//				return (_this.pha && _this.record && _this.carenets && _.map(_this.carenets, function(i, c) {return _this.phas[c.carenet_id] != null;}));
+//		}, function() {
+//				$('#app_content_iframe').fadeOut(function(){
+//				$('#app_content').fadeIn(function(){
+//						_this.render({to: 'app_content'});
+//				});
+//				});
 // 
-//         $('#pha_carenets_form').submit(function() {
-//         $(this).find('input[name=carenet]').each(function(i, checkbox) {
-//             var the_carenet = _.select(_this.carenets, function(i) {return _this.carenets[i].carenet_id == checkbox.value;})[0];
-//             if (checkbox.checked) {
-//             // add the pha to the carenet
-//             the_carenet.add_pha(_this.pha);
-//             } else {
-//             // remove the pha from the carenet
-//             the_carenet.remove_pha(_this.pha);
-//             }
-//         });
-//         setTimeout("HealthfeedController.dispatch('index')", 0);
-//         return false;
-//         });
-//     });
-//     },
+//				$('#pha_carenets_form').submit(function() {
+//				$(this).find('input[name=carenet]').each(function(i, checkbox) {
+//						var the_carenet = _.select(_this.carenets, function(i) {return _this.carenets[i].carenet_id == checkbox.value;})[0];
+//						if (checkbox.checked) {
+//						// add the pha to the carenet
+//						the_carenet.add_pha(_this.pha);
+//						} else {
+//						// remove the pha from the carenet
+//						the_carenet.remove_pha(_this.pha);
+//						}
+//				});
+//				setTimeout("HealthfeedController.dispatch('index')", 0);
+//				return false;
+//				});
+//		});
+//		},
 // });
 
 // 
