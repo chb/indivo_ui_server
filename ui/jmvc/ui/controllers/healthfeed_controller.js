509364e6e9b820c27efe5fccc7116ffd2de50409
Pascal Pfiffner <phase.of.matter@gmail.com>
2011-08-02 11:41:47 -0400

    Make healthfeed the selected tab when it auto-selects itself

--- ui/jmvc/ui/controllers/healthfeed_controller.js
+++ ui/jmvc/ui/controllers/healthfeed_controller.js
@@ -10,35 +10,34 @@
 $.Controller.extend('UI.Controllers.Healthfeed',
 /* @Static */
 {
-    onDocument: true,
-    show: function() {
-      ACCOUNT.get_healthfeed(function(notifications) {
-        hf_items = _.map(notifications, function(n){
-          // refactor me (IE doesn't like @attr, among many other things)
-          return {
-            'received_at': n.received_at,
-            'document': null,
-            'id': n['@id'],
-            'content': n.content,
-            'sender': n.sender,
-            'record': {
-              'id': n.record['@id'],
-              'label': n.record['@label']
-            }
-          }
-        })
-
-        $("#active_app_tabs").tabs('select', 0);
-        $('#app_content').html($.View('//ui/views/healthfeed/show.ejs',{'notifications': hf_items}))
-        $('#app_content_iframe').attr('src', 'about:blank').hide();
-        $('#app_content').show();
-      });
-    }
+		onDocument: true,
+		show: function() {
+			ACCOUNT.get_healthfeed(function(notifications) {
+				hf_items = _.map(notifications, function(n) {
+					// refactor me (IE doesn't like @attr, among many other things)
+					return {
+						'received_at': n.received_at,
+						'document': null,
+						'id': n['@id'],
+						'content': n.content,
+						'sender': n.sender,
+						'record': {
+							'id': n.record['@id'],
+							'label': n.record['@label']
+						}
+					}
+				})
+				
+				$("#active_app_tabs").tabs('select', 0);
+				$('#app_content').html($.View('//ui/views/healthfeed/show.ejs',{'notifications': hf_items}))
+				$('#app_content_iframe').attr('src', 'about:blank').hide();
+				$('#app_content').show();
+			});
+		}
 },
 /* @Prototype */
 {
-  ready: function(el, options){
-    UI.Controllers.Healthfeed.show();
-    $('#healthfeed_li').bind('click', UI.Controllers.Healthfeed.show); // use basic jquery .bind
-  }
+	ready: function(el, options){
+		$('#healthfeed_li').bind('click', UI.Controllers.Healthfeed.show).click();
+	}
 });
