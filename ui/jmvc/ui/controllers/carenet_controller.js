9ea67c8fceca082571ddd4d770ed3701a0fc4dbe
Pascal Pfiffner <phase.of.matter@gmail.com>
2011-08-09 16:30:39 -0400

    "Sharing" tab has now most functionality in the UI. Need to hook up with the backend.

--- ui/jmvc/ui/controllers/carenet_controller.js
+++ ui/jmvc/ui/controllers/carenet_controller.js
@@ -95,7 +95,7 @@ $.Controller.extend('UI.Controllers.Carenet',
 			
 			// show known accounts and carenets
 			self.updateAccounts();
-			$('#carenet_drag_accounts').show();
+			$('#carenet_drag_accounts').fadeIn('fast');
 			self.updateCarenets();
 		}
 	},
@@ -123,17 +123,16 @@ $.Controller.extend('UI.Controllers.Carenet',
 			helper: 'clone',
 			containment: '#app_content',
 			start: function(event, ui) {
+				ui.helper.addClass('account_dragged');
+				
+				// indicate (im)possible targets
 				var account_id = $(this).model().account_id;
 				$('#carenets').find('.carenet').not('.new').each(function(i, elem) {
-					var carenet = $(elem).model();
-					if (carenet && carenet.accounts && carenet.accounts.length > 0) {
-						var account_arr = carenet.accounts;
-						if (account_arr && _(account_arr).detect(function(a) { return a.account_id === account_id; })) {
-							$(elem).css('opacity', 0.4);
-						}
+					var account_arr = $(elem).model().accounts;
+					if (account_arr && account_arr.length > 0 && _(account_arr).detect(function(a) { return a.account_id === account_id; })) {
+						$(elem).css('opacity', 0.4);
 					}
 				});
-				ui.helper.addClass('account_dragged');
 			},
 			stop: function(event) {				// this may be called AFTER another 'start' if the user very quickly drags another app. We could use 'drag' instead of 'start'
 				$('#carenets').find('.carenet').each(function(i, elem) { $(elem).css('opacity', 1); });
@@ -187,9 +186,13 @@ $.Controller.extend('UI.Controllers.Carenet',
 				}
 			},
 			
-			// add app on drop
+			// add or transpfer account on drop
 			drop: function(event, ui) {
 				self.addAccountToCarenet(ui.draggable.model(), ui.helper, $(this));
+				
+				if (ui.helper.hasClass('draggable_will_remove')) {
+					self.removeAccountFromCarenet(ui.draggable.model(), ui.helper);
+				}
 			}
 		});
 		
@@ -199,17 +202,13 @@ $.Controller.extend('UI.Controllers.Carenet',
 		});
 	},
 	
-	setupCarenetAccount: function(account_view) {
-		
-	},
-	
 	
 	/**
 	 * Operations
 	 */
 	showNewAccountBox: function(ev, el, error_msg) {
 		var self = this;
-		el.addClass('active');
+		el.addClass('highlight');
 		var form = el.find('form');
 		if ('none' != form.css('display')) {
 			return;
@@ -257,7 +256,7 @@ $.Controller.extend('UI.Controllers.Carenet',
 	
 	hideNewAccountBox: function() {
 		var form = $('#new_account_form');
-		form.parent().removeClass('active');
+		form.parent().removeClass('highlight');
 		form.hide().siblings().show();
 	},
 	
@@ -285,9 +284,7 @@ $.Controller.extend('UI.Controllers.Carenet',
 	},
 	
 	addAccountToCarenet: function(account, dragged_view, carenet_view) {
-		var carenet = carenet_view.model();
 		carenet_view.addClass('expanded');
-		account.temporarily_added = true;
 		
 		// create the view at drop position
 		var carenet_content = carenet_view.find('.carenet_content').first();
@@ -307,13 +304,230 @@ $.Controller.extend('UI.Controllers.Carenet',
 		}
 		carenet_content.append(new_acc_view);
 		new_acc_view.animate({'left': coords.left, 'top': coords.top}, 'fast');
-		carenet_view.find('.carenet_num_apps').first().text('~');
 		
-		// add to array and tell the server
-		carenet.accounts.push(account);
-		carenet.add_account(account.account_id, this.callback('didAddAccountToCarenet', new_acc_view, carenet_view));
+		// tell the server
+		var carenet = carenet_view.model();
+		if (!carenet) {
+			console.log("DROPPED ON NEW CARENET");
+		}
+		else {
+			carenet_view.find('.carenet_num_items').first().text('~');
+			carenet.add_account(account.account_id, this.callback('didAddAccountToCarenet', new_acc_view, carenet_view));
+		}
 	},
-	didAddAccountToCarenet: function(data, textStatus, xhr) {
-		console.log(data);
+	didAddAccountToCarenet: function(account_view, carenet_view, data, textStatus, xhr) {
+		if ('success' == textStatus) {		// as of Indivo X b3, this always return success!
+			var carenet = carenet_view.model();
+			carenet.accounts.push(account_view.model());
+			carenet_view.find('.carenet_num_items').first().text(carenet.accounts.length);
+			
+			window.setTimeout(function() { carenet_view.removeClass('expanded'); }, 600);
+		}
+	},
+	
+	removeAccountFromCarenet: function(account, dragged_view) {
+		dragged_view.draggable('destroy');
+		
+		// tell the server
+		var carenet_view = dragged_view.parentsUntil('.carenet').last().parent();
+		var carenet = carenet_view.model();
+		
+		if (carenet) {
+			carenet_view.addClass('expanded');
+			carenet_view.find('.carenet_num_items').first().text('~');
+			carenet.remove_account(account.account_id, this.callback('didRemoveAccountFromCarenet', account, dragged_view, carenet_view));
+		}
+		else {
+			dragged_view.remove();
+		}
+		
+		// animate removal
+		if (!dragged_view.hasClass('draggable_will_transfer')) {
+			var parent = $('#app_content');
+			//var p_off = parent.offset();		// '#app_content' is no offsetParent! Will this change? If so, subtract p_off from v_off
+			var v_off = dragged_view.offset();
+			
+			UI.Controllers.MainController.poof(dragged_view);
+		}
+	},
+	didRemoveAccountFromCarenet: function(account, account_view, carenet_view, data, textStatus, xhr) {
+		if ('success' == textStatus) {		// as of Indivo X b3, this always returns "success"!
+			
+			// remove from accounts array
+			var carenet = carenet_view.model();
+			var acc_id = account.account_id;
+			if (carenet && carenet.accounts && carenet.accounts.length > 0) {
+				for (var i = 0; i < carenet.accounts.length; i++) {
+					var acc = carenet.accounts[i];
+					if (acc.account_id == acc_id) {
+						carenet.accounts.splice(i, 1);
+						break;
+					}
+				}
+			}
+			
+			// update view
+			if (0 == carenet.accounts.length) {
+				carenet_view.find('.carenet_content').first().append('<p class="hint">{% trans "No accounts" %}</p>');
+			}
+			account_view.remove();
+			carenet_view.find('.carenet_num_items').first().text(carenet.accounts.length);
+			
+			window.setTimeout(function() { carenet_view.removeClass('expanded'); }, 600);
+		}
+	},
+	
+	changeCarenetName: function(form) {
+		$('body').unbind('click');
+		
+		var new_name = form.find('input').first().val();
+		var carenet_view = form.parentsUntil('.carenet').last().parent();
+		var carenet = carenet_view.model();
+		
+		this.record.rename_carenet(carenet.carenet_id, new_name, this.callback('didChangeCarenetName', form, new_name));
+	},
+	didChangeCarenetName: function(name_form, new_name, data, textStatus, xhr) {
+		if ('success' == textStatus) {
+			name_form.parent().find('b').show();
+			
+			// return the form to a link
+			name_form.parent().find('a').text(new_name).show();		// would be cleaner to fetch the new name from the 'data' xml
+			name_form.remove();
+		}
+		else {
+			alert("There was an error changing the name, please try again\n\n" + data);
+			name_form.find('button').removeAttr('disabled');
+		}
+	},
+	
+	
+	/**
+	 * Utilities
+	 */
+	setupCarenetName: function(carenet_name_view) {
+		if (carenet_name_view) {
+			var self = this;
+			
+			carenet_name_view.click(function(event) {
+				event.stopPropagation();
+				$(this).parent().find('b').hide();
+				
+				// insert an input field on link click
+				var field = $('<input type="name" name="carenet_name" value="' + carenet_name_view.text() + '" />');
+				var submit = $('<button type="submit">Save</button>');
+				var form = $('<form/>', {'method': 'get', 'action': 'javascript:;'}).append(field).append(submit);
+				form.submit(function() {
+					var form = $(this);
+					form.find('button').attr('disabled', 'disabled');
+					self.changeCarenetName(form);
+					return false;
+				});
+				carenet_name_view.before(form).hide();
+				form.find('input').first().focus();
+				form.parent().click(function(event) { event.stopPropagation(); });
+				
+				// cancel input if we click somewhere outside
+				$('body').click(function(event) {
+					$(this).unbind('click');
+					var forms = $('#carenets').find('.carenet_inner').find('form');
+					if (forms.length > 0) {
+						forms.each(function(i) {
+							$(this).siblings().show();
+							$(this).remove();
+						});
+					}
+				});
+			});
+		}
+	},
+	
+	setupCarenetAccount: function(account_view) {
+		if (account_view) {
+			var self = this;
+			
+			//** show occount info on hover
+			account_view.bind('mouseover', function(event) {
+				var self_id = $(this).model().account_id;
+				$('#known_accounts').find('.account').not('.new').each(function(i) {
+					if ($(this).model().account_id == self_id) {
+						$(this).addClass('highlight');
+					}
+				});
+			})
+			.bind('mouseout', function(event) {
+				if (!$(this).hasClass('app_dragged')) {
+					$('#known_accounts').find('.account').removeClass('highlight');
+				}
+			})
+			
+			//** setup action on mouse up (can't use "stop" of draggable as this will first revert the item)
+			.bind('mouseup', function(event) {
+				var view = $(this);
+				
+				// remove from carenet
+				if (view.hasClass('draggable_will_remove')) {
+					view.draggable('destroy');
+					var account = view.model();
+					self.removeAccountFromCarenet(account, view);
+				}
+			})
+			
+			//** setup dragging
+			.bind('selectstart', function () { return false; })		// needed for WebKit (unless we upgrade jQuery UI to 1.8.6+)
+			.draggable({
+				distance: 8,
+				revert: true,
+				revertDuration: 300,
+				containment: '#app_content',
+				start: function(event, ui) {
+					var view = ui.helper;
+					view.addClass('account_dragged');
+					var carenet_view = view.parentsUntil('.carenet').last().parent();
+					carenet_view.addClass('expanded');
+					
+					// indicate (im)possible targets
+					var account_id = $(this).model().account_id;
+					$('#carenets').find('.carenet').not('.expanded').not('.new').each(function(i, elem) {
+						var account_arr = $(elem).model().accounts;
+						if (account_arr && account_arr.length > 0 && _(account_arr).detect(function(a) { return a.account_id === account_id; })) {
+							$(elem).css('opacity', 0.4);
+						}
+					});
+				},
+				drag: function(event, ui) {
+					var view = ui.helper;
+					var par = view.parent();
+					var parpos = par.position();
+					var parsize = {'width': par.outerWidth(true), 'height': par.outerWidth(true)};
+					var x = parseInt(view.css('left')) + view.outerWidth(true) / 2;
+					var y = parseInt(view.css('top')) + view.outerHeight(true) / 2;
+					var minx = - parpos.left;
+					var maxx = parsize.width + parpos.left;
+					var miny = - parpos.top;
+					var maxy = parsize.height;
+					
+					if (x < minx || x > maxx || y < miny || y > maxy) {
+						view.addClass('draggable_will_remove');
+					}
+					else {
+						view.removeClass('draggable_will_remove');
+					}
+				},
+				stop: function(event, ui) {
+					var view = ui.helper;
+					view.removeClass('app_dragged');
+					
+					// revert UI (note: this may be called AFTER another 'start' if the user very quickly drags another app since this is only called once the move-back animation finished)
+					$('#carenets').find('.carenet').each(function(i, elem) {
+						$(elem).css('opacity', 1);
+					});
+					$('#known_accounts').find('.accounts').removeClass('highlight');
+					
+					if (!view.hasClass('draggable_will_remove')) {
+						view.parentsUntil('.carenet').last().parent().removeClass('expanded');
+					}
+				}
+			});
+		}
 	}
 });
--- ui/jmvc/ui/models/carenet.js
+++ ui/jmvc/ui/models/carenet.js
@@ -5,43 +5,43 @@
 $.Model.extend('UI.Models.Carenet',
 /* @Static */
 {
-  from_xml_node: function(record_id, xml_node) {
-    return new UI.Models.Carenet({
-      'record_id': record_id,
-      'carenet_id': $(xml_node).attr('id'),
-      'name': $(xml_node).attr('name')
-    });
-  }
+	from_xml_node: function(record_id, xml_node) {
+		return new UI.Models.Carenet({
+			'record_id': record_id,
+			'carenet_id': $(xml_node).attr('id'),
+			'name': $(xml_node).attr('name')
+		});
+	}
 },
 /* @Prototype */
 /* magic attrs:
-   record_id
-   carenet_id
-   name
+	 record_id
+	 carenet_id
+	 name
  */
 {
-  get_people: function(callback) {
-    indivo_api_call("GET", '/carenets/' + this.carenet_id + '/accounts/', null, function(result) {
-      var account_objects_list = $(result).find('CarenetAccount').map(function(i, account_xml_node) {
-        return UI.Models.Account.from_xml_node(account_xml_node);
-      })
-      callback(account_objects_list);
-    })
-  },
-
-  add_pha: function(pha, callback) {
-    indivo_api_call("PUT", '/carenets/' + this.carenet_id + '/apps/' + encodeURIComponent(pha.id), null, callback);
-  },
-
-  remove_pha: function(pha, callback) {
-    indivo_api_call("DELETE", '/carenets/' + this.carenet_id + '/apps/' + encodeURIComponent(pha.id), null, callback);
-  },
-
-  add_account: function(account_id, callback) {
-    indivo_api_call("POST", '/carenets/' + this.carenet_id + '/accounts/', {'account_id' : account_id, 'write': 'true'}, callback);
-  },
-
-  remove_account: function(account_id, callback) {
-    indivo_api_call("DELETE", '/carenets/' + this.carenet_id + '/accounts/' + encodeURIComponent(account_id), null, callback);
-  }
+	get_people: function(callback) {
+		indivo_api_call("GET", '/carenets/' + this.carenet_id + '/accounts/', null, function(result) {
+			var account_objects_list = $(result).find('CarenetAccount').map(function(i, account_xml_node) {
+				return UI.Models.Account.from_xml_node(account_xml_node);
+			})
+			callback(account_objects_list);
+		})
+	},
+	
+	add_pha: function(pha, callback) {
+		indivo_api_call("PUT", '/carenets/' + this.carenet_id + '/apps/' + encodeURIComponent(pha.id), null, callback);
+	},
+	
+	remove_pha: function(pha, callback) {
+		indivo_api_call("DELETE", '/carenets/' + this.carenet_id + '/apps/' + encodeURIComponent(pha.id), null, callback);
+	},
+	
+	add_account: function(account_id, callback) {
+		indivo_api_call("POST", '/carenets/' + this.carenet_id + '/accounts/', {'account_id' : account_id, 'write': 'true'}, callback);
+	},
+	
+	remove_account: function(account_id, callback) {
+		indivo_api_call("DELETE", '/carenets/' + this.carenet_id + '/accounts/' + encodeURIComponent(account_id), null, callback);
+	}
 })
