7f4e0588a573a150e3cb5b9840172a17f06d2a0d
Daniel Haas <daniel.haas@childrens.harvard.edu>
2012-05-07 20:20:25 +0000

    Beautified some files using steal/cleanjs

--- ui/jmvc/ui/controllers/app_list_controller.js
+++ ui/jmvc/ui/controllers/app_list_controller.js
@@ -9,13 +9,14 @@ $.Controller.extend('UI.Controllers.AppList',
 /* @Static */
 {
 	defaults: {
-		selector: "li",				// selector for app selection 
+		// selector for app selection 
+		selector: "li",
 		animateTabSelection: true
 	}
 },
 /* @Prototype */
 {
-	init: function(params) {
+	init: function( params ) {
 		steal.dev.log("AppList Controller init");
 		this.account = this.options.account;
 		this.enabledApps = this.options.enabledApps;
@@ -24,93 +25,103 @@ $.Controller.extend('UI.Controllers.AppList',
 		this.appMap = {};
 		this.lastAppId = null;
 	},
-	
-	/*
+
+/*
 	 * Display an app when its selector is clicked.  
 	 */
-	"{selector} click": function(el, ev) {
-	        var appModel = el.model();
+	"{selector} click": function( el, ev ) {
+		var appModel = el.model();
 		var appId = el.attr("id");
-	        var controller = el.attr("data-controller");
+		var controller = el.attr("data-controller");
 
 		// animate tab to new selection TODO: do we want to revert on failure?
-		this.selectTab(el);		
+		this.selectTab(el);
 
 		// Clean up the previous running app
 		$("#app_content").children().hide();
-		if (this.lastAppId) {
-		    // if the click was on the previously running app, then refresh that app
-		    if (appId === this.lastAppId && this.appMap[appId]) {
+		if ( this.lastAppId ) {
+			// if the click was on the previously running app, then refresh that app
+			if ( appId === this.lastAppId && this.appMap[appId] ) {
 
-			// if the previously selected tab was internal, clean up its div
-			if (!appModel) { this.appMap[appId].remove(); }
-		
-			// otherwise, let the app_manager handle it
-			else { APP_MANAGER.stop_managing_app(appModel.MANAGER_uuid);}
-			
-			// and remove it from our app map
-			delete this.appMap[appId];
-		    }
-		     
-		    // Otherwise, Background the previous running app
-		    else {
-			var lastModel = $("#" + this.lastAppId).model();
-			if (lastModel) {
-			    APP_MANAGER.notify_app_backgrounded(lastModel.MANAGER_uuid);
+				// if the previously selected tab was internal, clean up its div
+				if (!appModel ) {
+					this.appMap[appId].remove();
+				}
+
+				// otherwise, let the app_manager handle it
+				else {
+					APP_MANAGER.stop_managing_app(appModel.MANAGER_uuid);
+				}
+
+				// and remove it from our app map
+				delete this.appMap[appId];
 			}
-		    }
+
+			// Otherwise, Background the previous running app
+			else {
+				var lastModel = $("#" + this.lastAppId).model();
+				if ( lastModel ) {
+					APP_MANAGER.notify_app_backgrounded(lastModel.MANAGER_uuid);
+				}
+			}
 		}
 
 		// If the newly selected app is already running and has an associated model,
 		// foreground it and show it		
 		var appEl = this.appMap[appId];
-		if (appEl && appModel) {
-		    APP_MANAGER.notify_app_foregrounded(appModel.MANAGER_uuid);
-		    appEl.show()
+		if ( appEl && appModel ) {
+			APP_MANAGER.notify_app_foregrounded(appModel.MANAGER_uuid);
+			appEl.show()
 		}
 
 		// If the newly selected app is already running and internal, just show it.
-		else if (appEl) {
-		    appEl.show()
+		else if ( appEl ) {
+			appEl.show()
 		}
-		
+
 		// If we are starting an app for the first time, and it has an associated model, 
 		// set it up with the app manager
-		else if (appModel) {
+		else if ( appModel ) {
 
-		    // show a loading image while we wait
-		    var loading_img = el.find(".app_tab_loading");
-		    loading_img.show()
-		    
-		    var activeRecord = this.account.attr("activeRecord");
-		    var manifest = appModel.getManifest({'record_id': activeRecord.carenet_id ? '' : activeRecord.id,
-							 'carenet_id': activeRecord.carenet_id || ''});
-		    var context = this.getContext();
-		    var self = this;
-		    var ret = APP_MANAGER.launch_app(manifest, context, {})
-		    ret.done(function(app_instance){
-			    self.appMap[appId] = $(app_instance.iframe).load(function(){loading_img.hide();});
-			    appModel.attr("MANAGER_uuid", app_instance.uuid);
+			// show a loading image while we wait
+			var loading_img = el.find(".app_tab_loading");
+			loading_img.show()
+
+			var activeRecord = this.account.attr("activeRecord");
+			var manifest = appModel.getManifest({
+				'record_id': activeRecord.carenet_id ? '' : activeRecord.id,
+				'carenet_id': activeRecord.carenet_id || ''
+			});
+			var context = this.getContext();
+			var self = this;
+			var ret = APP_MANAGER.launch_app(manifest, context, {})
+			ret.done(function( app_instance ) {
+				self.appMap[appId] = $(app_instance.iframe).load(function() {
+					loading_img.hide();
+				});
+				appModel.attr("MANAGER_uuid", app_instance.uuid);
 			});
 		}
-		
+
 		// If we are starting an app for the first time, and it is internal,
 		// just load the html under the #app_content div and show it.
 		else {
-		    // show a loading image while we wait
-		    var loading_img = el.find(".app_tab_loading");
-		    loading_img.show()
+			// show a loading image while we wait
+			var loading_img = el.find(".app_tab_loading");
+			loading_img.show()
 
-		    var newHtml = $('<div></div>')["ui_" + controller]({account:this.account});
-		    this.appMap[appId] = newHtml.appendTo("#app_content").show();
+			var newHtml = $('<div></div>')["ui_" + controller]({
+				account: this.account
+			});
+			this.appMap[appId] = newHtml.appendTo("#app_content").show();
 
-		    loading_img.hide()
+			loading_img.hide()
 		}
 
 		// set the newly active app as the latest active one
 		this.lastAppId = appId;
 	},
-	
+
 	/**
 	 * Listen for new apps being added to enabledApps and renders a new selector
 	 * for them
@@ -134,62 +145,71 @@ $.Controller.extend('UI.Controllers.AppList',
 	 * <div>this is another bg app</div>
 	 * @codeend
 	 */
-	"{enabledApps} add": function(list, ev, newApps) {
+	"{enabledApps} add": function( list, ev, newApps ) {
 		var activeRecord = this.account.attr("activeRecord");
-		if (activeRecord) {
-			$.each(newApps, function(i, app) {
+		if ( activeRecord ) {
+			$.each(newApps, function( i, app ) {
 				// app with a UI
-				if (app.has_ui) {
+				if ( app.has_ui ) {
 					var startURL = app.getStartURL({
 						'record_id': activeRecord.carenet_id ? '' : activeRecord.id,
 						'carenet_id': activeRecord.carenet_id || ''
 					});
-					$('#active_app_tabs').append($.View("//ui/views/pha/app_tab", {isBackgroundApp:false, app:app, startURL:startURL}));
+					$('#active_app_tabs').append($.View("//ui/views/pha/app_tab", {
+						isBackgroundApp: false,
+						app: app,
+						startURL: startURL
+					}));
 				}
-		
+
 				// background app
 				else {
-					$('#background_app_tabs').append($.View("//ui/views/pha/app_tab", {isBackgroundApp:true, app:app}));
+					$('#background_app_tabs').append($.View("//ui/views/pha/app_tab", {
+						isBackgroundApp: true,
+						app: app
+					}));
 				}
 			});
 		}
 	},
-	
-	/*
+
+/*
 	 * Listen for apps being removed from enabledApps and removes them from the
 	 * display
 	 */
-	"{enabledApps} remove": function(list, ev, removedApps) {
+	"{enabledApps} remove": function( list, ev, removedApps ) {
 		removedApps.elements(this.element).remove();
 	},
-	
-	/*
+
+/*
 	 * Listens for changes to the account's active Record, and loads the enabled
 	 * apps for the new active Record 
 	 */
-	"{account} updated.attr": function(account, ev, attr, newVal) {
+	"{account} updated.attr": function( account, ev, attr, newVal ) {
 		//TODO: JMVC 3.3 should merge $.Observable with $.Model, so we can listen for changes to a specific attribute
 		// load record's apps
-		if (attr === "activeRecord") {
-			if (newVal) {
+		if ( attr === "activeRecord" ) {
+			if ( newVal ) {
 				UI.Controllers.MainController.unlockAppSelector();
 				var record = newVal;
 
 				// stop managing the old record's apps
 				APP_MANAGER.record_context_changed();
-				$.each(this.appMap, function(app_id, app_el) {
-					if (!app_el.model()) { app_el.remove(); }
-				    });
+				$.each(this.appMap, function( app_id, app_el ) {
+					if (!app_el.model() ) {
+						app_el.remove();
+					}
+				});
 				this.appMap = {};
 
 				// is this a carenet or a record? depending on which, init the appropriate apps
-				if (record.carenet_id) {
+				if ( record.carenet_id ) {
 					UI.Models.PHA.get_by_carenet(record.carenet_id, null, this.callback('set_enabled_apps'));
 				} else {
 					UI.Models.PHA.get_by_record(record.id, null, this.callback('set_enabled_apps'));
 				}
 			}
-			
+
 			// if we got no record, e.g. to show the new-record form
 			else {
 				UI.Controllers.MainController.lockAppSelector();
@@ -198,31 +218,32 @@ $.Controller.extend('UI.Controllers.AppList',
 		}
 	},
 
-	/*
+/*
 	 * Set the current enabledApps List, clearing out the previous
 	 * @param {UI.Model.PHA.List} apps List of apps to show as enabled
 	 */
-	set_enabled_apps: function(apps) {
+	set_enabled_apps: function( apps ) {
 		var enabledList = this.enabledApps,
-			selected_id = $('#app_selector .selected').attr('id'), // remember selected app before clearing...
-			removeList = enabledList.slice(0, enabledList.length); 
-			
+			selected_id = $('#app_selector .selected').attr('id'),
+			// remember selected app before clearing...
+			removeList = enabledList.slice(0, enabledList.length);
+
 		// clear out exising apps
-		$.each(removeList, function(i, app) { 
+		$.each(removeList, function( i, app ) {
 			enabledList.remove(app.id);
-		    });		
-		
+		});
+
 		// add new ones...
-		$.each(apps, function(index, value){
+		$.each(apps, function( index, value ) {
 			enabledList.push(value);
-		    });
-		
+		});
+
 		// TODO: is keeping track of previously selected app something we really want to do?
 		// Yes, otherwise switching a record will always throw you to the healthfeed, which I think is not desireable (pp)
 		// ...and try to re-select previous app, show healthfeed otherwise
-		if (this.account.activeRecord) {
+		if ( this.account.activeRecord ) {
 			var old_app = $('#' + selected_id);
-			if (old_app.is('*')) {
+			if ( old_app.is('*') ) {
 				old_app.click();
 			}
 			else {
@@ -230,13 +251,13 @@ $.Controller.extend('UI.Controllers.AppList',
 			}
 		}
 	},
-	
-	/*
+
+/*
 	 * Clear out any previous Controllers that have been attached to given element
 	 * @param {Object} el element to remove Controllers from
 	 */
-	clearControllers: function(el) {
-		$.each($(el).controllers(), function(i, val) {
+	clearControllers: function( el ) {
+		$.each($(el).controllers(), function( i, val ) {
 			val.destroy();
 		});
 	},
@@ -244,63 +265,62 @@ $.Controller.extend('UI.Controllers.AppList',
 	/**
 	 * Simple tab functionality
 	 */
-	selectTab: function(el) {
+	selectTab: function( el ) {
 		var selector = this.element,
 			selected = selector.find(this.selector + '.selected'),
 			activeRecord = this.account.attr("activeRecord"),
 			bgcolor = activeRecord ? activeRecord.bgcolor : 'rgb(250,250,250)';
-		
+
 		// deselect old tab and select new tab
-		if (selected.is('*') && (!el || selected.attr('id') != el.attr('id'))) {
+		if ( selected.is('*') && (!el || selected.attr('id') != el.attr('id')) ) {
 			var sel_clone = selected.clone(false).css({
-				       'position': 'absolute',
-				           'left': '10px',
-				          'right': '-1px',
-				            'top': selected.position().top + 'px',
-				         'height': selected.innerHeight() - 8 + 'px',		/* 8 = 4px + 4px top and bottom padding. Would be better to get this from CSS or calculate it! */
-				'-moz-box-shadow': 'none', '-webkit-box-shadow': 'none', 'box-shadow': 'none'
+				'position': 'absolute',
+				'left': '10px',
+				'right': '-1px',
+				'top': selected.position().top + 'px',
+				'height': selected.innerHeight() - 8 + 'px',
+				/* 8 = 4px + 4px top and bottom padding. Would be better to get this from CSS or calculate it! */
+				'-moz-box-shadow': 'none',
+				'-webkit-box-shadow': 'none',
+				'box-shadow': 'none'
 			}).empty();
 			selected.removeClass('selected').css('background-color', '');
-			
+
 			// animate tab selection
-			if (el) {
+			if ( el ) {
 				el.css('background-color', 'transparent').css('border-right-color', 'transparent');
-				if (this.animateTabSelection) {
-					selected.parent().parent().children().first().prepend(sel_clone);		// prepend to first <ul> to stack it behind all other tabs
+				if ( this.animateTabSelection ) {
+					selected.parent().parent().children().first().prepend(sel_clone); // prepend to first <ul> to stack it behind all other tabs
 					sel_clone.animate({
-							   'top': el.position().top + 'px',
-							'height': el.innerHeight() - 8 + 'px'
-						},
-						300,
-						'swing',
-						function() {
-							var bgcolor = activeRecord ? activeRecord.bgcolor : 'rgb(250,250,250)';
-							el.addClass('selected').css('background-color', bgcolor).css('border-right-color', '');
-							$(this).remove();
-						}
-					);
+						'top': el.position().top + 'px',
+						'height': el.innerHeight() - 8 + 'px'
+					}, 300, 'swing', function() {
+						var bgcolor = activeRecord ? activeRecord.bgcolor : 'rgb(250,250,250)';
+						el.addClass('selected').css('background-color', bgcolor).css('border-right-color', '');
+						$(this).remove();
+					});
 					return;
 				}
 			}
 		}
-		
+
 		// set background color on newly selected tab
-		if (el) {
+		if ( el ) {
 			el.addClass('selected').css('background-color', bgcolor);
 		}
 	},
 
 	getContext: function() {
-	    var activeRecord = this.account.attr("activeRecord");
-	    var context = {}
-	    context.user = {
-		id: this.account.id,
-		full_name: this.account.fullName
-	    };
-	    context.record = {
-		id: activeRecord.carenet_id || activeRecord.id,
-		full_name: activeRecord.label
-	    };
-	    return context;
+		var activeRecord = this.account.attr("activeRecord");
+		var context = {}
+		context.user = {
+			id: this.account.id,
+			full_name: this.account.fullName
+		};
+		context.record = {
+			id: activeRecord.carenet_id || activeRecord.id,
+			full_name: activeRecord.label
+		};
+		return context;
 	}
 });
--- ui/jmvc/ui/models/pha.js
+++ ui/jmvc/ui/models/pha.js
@@ -7,18 +7,20 @@
  */
 UI.Models.IndivoBase.extend('UI.Models.PHA',
 /* @Static */
-{	
+{
 
-    model: function(data) {
-        // custom converter for incoming models
-        data['app_id'] = data['id'];
-	    data["id"] = UI.UTILS.generateID();
-        return this._super(data);  
-    },
-    
-	get_by_record: function(record_id, type, success, error) {
-	        var url = this.apiBase() + 'records/' + encodeURIComponent(record_id) + '/apps/';
-		if (type) { url += "?type=" + encodeURIComponent(type); }
+	model: function( data ) {
+		// custom converter for incoming models
+		data['app_id'] = data['id'];
+		data["id"] = UI.UTILS.generateID();
+		return this._super(data);
+	},
+
+	get_by_record: function( record_id, type, success, error ) {
+		var url = this.apiBase() + 'records/' + encodeURIComponent(record_id) + '/apps/';
+		if ( type ) {
+			url += "?type=" + encodeURIComponent(type);
+		}
 		return $.ajax({
 			url: url,
 			type: 'get',
@@ -27,47 +29,47 @@ UI.Models.IndivoBase.extend('UI.Models.PHA',
 			error: error
 		});
 	},
-	
-	get_by_carenet: function(carenet_id, type, success, error) {
+
+	get_by_carenet: function( carenet_id, type, success, error ) {
 		var url = this.apiBase() + 'carenets/' + encodeURIComponent(carenet_id) + '/apps/';
-		if(type) {
+		if ( type ) {
 			url += "?type=" + encodeURIComponent(type);
 		}
 		return $.ajax({
-			url : url,
-			type : 'get',
-			dataType : 'json pha.models',
-			success : success,
-			error : error
+			url: url,
+			type: 'get',
+			dataType: 'json pha.models',
+			success: success,
+			error: error
 		});
 	},
 
-    get_all: function(success, error) {
+	get_all: function( success, error ) {
 		return $.ajax({
-			url : this.apiBase() + 'apps/',
-			type : 'get',
-            dataType : 'json pha.models',
-			success : success,
-			error : error
-		});
-    },
-	
-	get: function(record_id, pha_id, success, error) {
-		var url = this.apiBase() + 'records/' + encodeURIComponent(record_id) + '/apps/' + encodeURIComponent(pha_id);
-		return $.ajax({
-			url : url,
-			type : 'get',
-			dataType : 'json pha.model',
-			success : success,
-			error : error
+			url: this.apiBase() + 'apps/',
+			type: 'get',
+			dataType: 'json pha.models',
+			success: success,
+			error: error
 		});
 	},
-	
-	authorize_token: function(token, record_id, success) {
-		var postURL = '/oauth/authorize';		// should we really POST directly to OAuth?
+
+	get: function( record_id, pha_id, success, error ) {
+		var url = this.apiBase() + 'records/' + encodeURIComponent(record_id) + '/apps/' + encodeURIComponent(pha_id);
+		return $.ajax({
+			url: url,
+			type: 'get',
+			dataType: 'json pha.model',
+			success: success,
+			error: error
+		});
+	},
+
+	authorize_token: function( token, record_id, success ) {
+		var postURL = '/oauth/authorize'; // should we really POST directly to OAuth?
 		var dict = {
 			'oauth_token': token,
-			  'record_id': record_id
+			'record_id': record_id
 		};
 		$.ajax({
 			type: 'post',
@@ -76,20 +78,20 @@ UI.Models.IndivoBase.extend('UI.Models.PHA',
 			complete: success
 		});
 	},
-	
+
 	//TODO: move to Record
-	delete_pha: function(record_id, pha_id, success, error) {
+	delete_pha: function( record_id, pha_id, success, error ) {
 		return $.ajax({
-			url : this.apiBase() + 'records/' + encodeURIComponent(record_id) + '/apps/' + encodeURIComponent(pha_id),
-			type : 'delete',
-			success : success,
-			error : error
+			url: this.apiBase() + 'records/' + encodeURIComponent(record_id) + '/apps/' + encodeURIComponent(pha_id),
+			type: 'delete',
+			success: success,
+			error: error
 		});
 	},
 
-	interpolate_url_template: function(url_template, vars) {
+	interpolate_url_template: function( url_template, vars ) {
 		var result = url_template;
-		$.each(vars, function(var_name, var_value) {
+		$.each(vars, function( var_name, var_value ) {
 			var regexp = new RegExp("{" + var_name + "}");
 			result = result.replace(regexp, var_value);
 		});
@@ -98,26 +100,26 @@ UI.Models.IndivoBase.extend('UI.Models.PHA',
 },
 
 /* @Prototype */
-{	
+{
 	getImageSource: function() {
-        return this.icon;
+		return this.icon;
 	},
-	
-	getStartURL: function(vars) {
-	    return this.Class.interpolate_url_template(this.index, vars);
-        },
-	
-        getManifest: function(context_vars) {
-	    return {
-		name: this.name,
-		description: this.description,
-		author: this.author,
-		id: this.app_id,
-		version: this.version,
-		mode: this.mode,
-		scope: this.scope,
-		index: this.getStartURL(context_vars),
-		icon: this.getImageSource()
-	    }
-        }
+
+	getStartURL: function( vars ) {
+		return this.Class.interpolate_url_template(this.index, vars);
+	},
+
+	getManifest: function( context_vars ) {
+		return {
+			name: this.name,
+			description: this.description,
+			author: this.author,
+			id: this.app_id,
+			version: this.version,
+			mode: this.mode,
+			scope: this.scope,
+			index: this.getStartURL(context_vars),
+			icon: this.getImageSource()
+		}
+	}
 });
